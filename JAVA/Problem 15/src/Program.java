2dc00661502039e751f5be671a54739dd9bdbcbe
RickyBensics <rbensics@yahoo.com>
2017-01-07 16:27:35 -0500

    Comments added

--- JAVA/Problem 15/src/Program.java
+++ JAVA/Problem 15/src/Program.java
@@ -1,3 +1,12 @@
+/**
+ * Starting in the top left corner of a 2×2 grid, and only being able to move to the right and down, 
+ * there are exactly 6 routes to the bottom right corner.
+ * 
+ * How many such routes are there through a 20×20 grid?
+ * 
+ * Richard Bensics
+ * 1/7/17
+ */
 
 public class Program 
 {
@@ -6,6 +15,7 @@ public class Program
 		int gridSize = 20;
 		long[][] grid = new long[gridSize+1][gridSize+1];
 		
+		//Pascal's triangle using dynamic programming
 		for(int i = 0; i < gridSize+1; i++)
 		{
 			grid[0][i] = 1;
